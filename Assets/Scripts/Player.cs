58c85f77a16e4e145a59f603056d2c85a0b79dcd
Alban Bleicher <alban.b@thewalkingnerds.com>
2021-12-02 10:53:01 +0100

    :sparkles: Camera movements  using Cinemachine

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,27 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float cameraSpeed = 2f;
-  private Animator animator;
-  private float pitch = 0.0f;
+    // Speed of Camera Movement
+    private Animator animator;
+    private NavMeshAgent agent;
+    private float pitch = 0.0f;
 
-void Start(){
-  animator=GetComponent<Animator>();
-}
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
 
+    }
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
- var velocity = transform.forward * Input.GetAxis("Vertical");
-      GetComponent<NavMeshAgent>().destination = transform.position+velocity;
-
-
-        animator.SetFloat("Speed", velocity.magnitude);
-            pitch-= Input.GetAxis("Mouse X") * cameraSpeed;
-         transform.eulerAngles = new Vector3(0.0f, pitch, 0.0f);
-
+        Vector3 move = transform.forward * z * agent.speed;
+        transform.Rotate(0f, x, 0f);
+        agent.Move(move * Time.deltaTime);
+        animator.SetFloat("Speed", move.magnitude);
     }
 }
